b287fa734a1b2011fd42184674e8bc2c632ec9f6
Artemiy228 <t3ma2002@mail.ru>
2020-08-09 01:45:28 +0300

    need adaptation

--- js/main.js
+++ js/main.js
@@ -1,3 +1,9 @@
-$(function(){
-
+$(function () {
+	$('.slider__inner').slick({
+		arrows: true,
+		autoplay: true,
+		auto: 3000,
+		prevArrow: '<button class="slider-btn slider-btn--left"><img src="images/Arrow-left.svg"></button>',
+		nextArrow: '<button class="slider-btn slider-btn--right"><img src="images/Arrow-right.svg"></button>',
+	});
 });
